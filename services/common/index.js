fbd9a5ae45ea6a0863ff6d57ee48c013af7dede6
Vasco Correia <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-10-05 19:38:23 +0100

    Add common package with middleware for authentication and authorization

---
+++ services/common/index.js
@@ -0,0 +1,21 @@
+// const Authenticator = (req, res) => {
+//   const token = req.headers.authorization;
+// };
+
+// const Authorizator = (req, res) => { };
+//
+const Authenticator = (url) => ((req, res, next) => {
+  console.log("Authenticating");
+  next()
+});
+
+const Authorizator = (url) => ((req, res, next) => {
+  console.log("Checking for Authorization");
+  next()
+});
+
+
+module.exports = {
+  Authenticator,
+  Authorizator
+}
