dbfc1b04aff1885ffc459ac05a05c60d86cd495f
Diogo Santos <diogo.santos@rnl.tecnico.ulisboa.pt>
2023-10-15 21:47:35 +0100

    pretty awaits

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,100 +1,131 @@
-import { Box, TextField, Button, Link, Typography, Paper } from '@mui/material';
+import { Box, TextField, Button, Typography, Paper, Grid } from "@mui/material";
 import { useNavigate, Navigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 
 const Login = () => {
+  /* Forward logged in user to main page */
+  const isLoggedIn = false;
 
-    { /* Forward logged in user to main page */ }
-    const isLoggedIn = false;
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	
-	const [sending, setSending] = useState(false);
-	
-	const [error, setError] = useState("");
+  const [sending, setSending] = useState(false);
 
-    if (isLoggedIn)
-        return <Navigate to="/upload" />
+  const [error, setError] = useState("");
 
-    const handleSubmit = async () => {
-        setSending(true);
-    
-        if (!username || !password) {
-            setSending(false);
-            return;
-        }
-        
-        try {
-            const response = await axios.post("/api/auth/create", {
-                email: username,
-                password: password,
-            });
-        } catch (err) {
-            setError(err.message);
-        } finally {
-            setSending(false);
-        }
-    };
+  if (isLoggedIn) return <Navigate to="/upload" />;
 
-    /* Create a box in the middle with a login form using MUI Material */
-    return ( 
-        <Paper sx={{ p:5 }}>
-			<Box sx={{ mx: "auto", width: "50vh" }}>
-				<Typography variant="h4" component="h1">
-					<strong>Login</strong>
-				</Typography>
-				{error && (
-					<Typography fontSize={15} color="red">
-						There was an error with your login: {error}
-					</Typography>
-				)}
-				<Box component="form">
-					<TextField
-						margin="normal"
-						variant="filled"
-						required
-						fullWidth
-						id="username"
-						label={"Username"}
-						name="username"
-						autoComplete="username"
-						autoFocus
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-					<TextField
-						margin="normal"
-						variant="filled"
-						required
-						fullWidth
-						name="password"
-						label={"Password"}
-						type="password"
-						id="password"
-						autoComplete="current-password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<Button
-						fullWidth
-						onClick={handleSubmit}
-						variant="contained"
-						sx={{ mt: 3 }}
-						disabled={!username || !password}
-					>
-						{sending ? "Sending..." : "Submit"}
-					</Button>
-					<Button color="primary" fullWidth sx={{ mt: 2 }}>
-						<Link to={"/register"}>New Account</Link>
-					</Button>
-				</Box>
-			</Box>
-        </Paper>
-    );
-}
+  const handleLoginSubmit = async () => {
+    setSending(true);
+
+    if (!username || !password) {
+      setSending(false);
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3001/auth", {
+        email: username,
+        password: password,
+      });
+      console.log(response);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setSending(false);
+    }
+  };
+  const handleCreateSubmit = async () => {
+    setSending(true);
+
+    if (!username || !password) {
+      setSending(false);
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3001/create", {
+        email: username,
+        password: password,
+      });
+      console.log(response);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setSending(false);
+    }
+  };
+
+  /* Create a box in the middle with a login form using MUI Material */
+  return (
+    <Paper sx={{ p: 5 }}>
+      <Box sx={{ mx: "auto", width: "50vh" }}>
+        <Typography variant="h4" component="h1">
+          <strong>Login</strong>
+        </Typography>
+        {error && (
+          <Typography fontSize={15} color="red">
+            There was an error with your login or account creation: {error}
+          </Typography>
+        )}
+        <Box component="form">
+          <TextField
+            margin="normal"
+            variant="filled"
+            required
+            fullWidth
+            id="username"
+            label={"Username"}
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            variant="filled"
+            required
+            fullWidth
+            name="password"
+            label={"Password"}
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Grid container spacing={2}>
+            <Grid item>
+              <Button
+                fullWidth
+                onClick={handleLoginSubmit}
+                variant="contained"
+                sx={{ mt: 3 }}
+                disabled={!username || !password}
+              >
+                {sending ? "Sending..." : "Login"}
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                fullWidth
+                onClick={handleCreateSubmit}
+                variant="contained"
+                sx={{ mt: 3 }}
+                disabled={!username || !password}
+              >
+                {sending ? "Sending..." : "Create"}
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Paper>
+  );
+};
 
 export default Login;
--- services/auth/src/index.js
+++ services/auth/src/index.js
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import fs from "fs";
+import cors from "cors";
 
 import { User } from "./models.js";
 import db from "./db.js";
@@ -16,59 +17,51 @@ const PRIV_KEY = fs.readFileSync(process.env.AUTH_PRIV_KEY);
 const JWT_EXPIRATION = 30 * 60 * 1000;
 
 const app = express();
+app.use(cors()); // for local development
 app.use(bodyParser.json());
 
 app.post("/create", async (req, res) => {
   // assume request is correct, might be problematic
   const { email, password } = req.body;
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      User.create({
-        email,
-        password: hash,
-      })
-        .then(() => {
-          res.status(200).send();
-        })
-        .catch((err) => {
-          res.status(400).send({ error: err });
-        });
-    })
-    .catch((err) => {
-      res.status(400).send({ error: err });
-    });
+  try {
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({email, password: hash});
+    res.status(200).send();
+  } catch(err) {
+    res.status(400).send({ error: err });
+  }
 });
 
 app.post("/auth", async (req, res) => {
   // assume request is correct, might be problematic
   const { email, password } = req.body;
-  User.findOne({ email: email })
-    .then((user) => {
-      bcrypt
-        .compare(password, user.password)
-        .then((valid) => {
-          if (valid) {
-            // generate jwt and send it back to the user
-            const expiresAt = new Date();
-            expiresAt.setTime(expiresAt.getTime() + JWT_EXPIRATION);
-            const token = jwt.sign(
-              {
-                email: user.email,
-                expiresAt: expiresAt.getTime(),
-              },
-              PRIV_KEY,
-              { algorithm: "RS256" }
-            );
-            res.status(200).send(token);
-          } else {
-            res.status(400).send({ error: "Authentication" });
-          }
-        })
-        .catch((err) => res.status(400).send({ error: err }));
-    })
-    .catch((err) => res.status(400).send({ error: err }));
+
+  try {
+    const user = await User.findOne({ email: email });
+    // this await is necessary
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      console.log("here");
+      // generate jwt and send it back to the user
+      const expiresAt = new Date();
+      expiresAt.setTime(expiresAt.getTime() + JWT_EXPIRATION);
+      const token = jwt.sign(
+        {
+          email: user.email,
+          expiresAt: expiresAt.getTime(),
+        },
+        PRIV_KEY,
+        { algorithm: "RS256" }
+      );
+
+      res.status(200).send(token);
+    } else {
+      res.status(401).send({ error: "Invalid credentials" });
+    }
+  } catch(err) {
+    res.status(400).send({ error: err });
+  }
 });
 
 // TODO: should be sent on login
