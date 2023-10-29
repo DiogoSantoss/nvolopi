915a154a7dd970137781bac27da29cfd93ea16cd
Vasco Correia <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-10-29 16:17:26 +0000

    Try to connect to DB until success

--- services/auth/src/db.js
+++ services/auth/src/db.js
@@ -2,19 +2,22 @@ import mongoose from "mongoose";
 
 const connect = () => {
   // Connecting to the database
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected");
-    })
-    .catch((error) => {
-      console.log("Error when connecting to DB");
-      console.error(error);
-      process.exit(1);
-    });
+  let done = false
+  while(!done)  {
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected");
+        done = true
+      })
+      .catch((error) => {
+        console.log("Error when connecting to DB");
+        console.error(error);
+      });
+  }
 };
 
 const db = {
